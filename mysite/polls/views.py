a82dc71b8f6ff6724e0e2fbe92c1840591adfbb5
osama-abuomar <osamana@outlook.com>
2016-01-18 14:09:23 +0200

    tests and bug fixing
    making some automated tests and discover bugs and solve them

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from .models import Choice, Question
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
@@ -11,7 +12,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         #"""Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5] 
+        return Question.objects.filter(pub_date__lte = timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
