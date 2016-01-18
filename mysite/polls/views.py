9086c367686faf626502cba980315e13a1728470
osama-abuomar <osamana@outlook.com>
2016-01-18 11:28:08 +0200

    fix mistake with the vote view
    changed the code by mistake
    * the project works completely fine at this stage

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -10,7 +10,7 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
+        #"""Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5] 
 
 
@@ -25,5 +25,19 @@ class ResultsView(generic.DetailView):
 
 
 def vote(request, question_id):
-     model = Question
-    template_name = 'polls/vote.html'
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):  # @UndefinedVariable
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
